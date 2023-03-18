883173414d4ab6f8f92be358694aef564405dc76
Ivaylo Petrov <isivaylopetrov@gmail.com>
2023-03-18 13:05:08 +0200

    google maps integration

--- App.tsx
+++ App.tsx
@@ -1,25 +1,5 @@
-import { StyleSheet, View } from 'react-native'
-import MapView from 'react-native-maps'
 import Home from './src/screens/Home'
 
 export default function App() {
-  return (
-    // <View style={styles.container}>
-    <Home />
-    // {/* <MapView style={styles.map} /> */}
-    // </View>
-  )
+  return <Home />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    width: '100%',
-    height: '100%',
-  },
-})
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,48 +1,400 @@
-import React, { useEffect, useState } from 'react'
-import { StyleSheet } from 'react-native'
-import MapView, { Marker } from 'react-native-maps'
-import * as Location from 'expo-location'
+import React, { useRef } from 'react'
+import { StyleSheet, View } from 'react-native'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+
+const DEFAULT_LATITUDE = 42.67444429848731
+const DEFAULT_LONGITUDE = 23.330522262810607
 
 export default function Map() {
-  const [location, setLocation] = useState<Location.LocationObject | null>(null)
-  console.log(location)
-  useEffect(() => {
-    ;(async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync()
-      if (status !== 'granted') {
-        return
-      }
-
-      let currentLocation = await Location.getCurrentPositionAsync({})
-      setLocation(currentLocation)
-    })()
-  }, [])
+  const mapViewRef = useRef<MapView | null>(null)
 
   return (
-    <MapView
-      style={styles.map}
-      initialRegion={{
-        latitude: location?.coords?.latitude || 42.67444429848731,
-        longitude: location?.coords?.longitude || 23.330522262810607,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    >
-      <Marker
-        coordinate={{
-          latitude: location?.coords?.latitude || 0,
-          longitude: location?.coords?.longitude || 0,
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        style={styles.map}
+        ref={mapViewRef}
+        initialRegion={{
+          longitude: DEFAULT_LONGITUDE,
+          latitude: DEFAULT_LATITUDE,
+          latitudeDelta: 0.0622,
+          longitudeDelta: 0.0221,
         }}
-        title="You are here"
-        description="This is your current location"
-      />
-    </MapView>
+        customMapStyle={customMapStyle}
+      ></MapView>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+
+    position: 'relative',
+  },
   map: {
     width: '100%',
     height: '100%',
   },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 64,
+    right: 24,
+  },
+  gpsButton: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    borderRadius: 50,
+    padding: 8,
+  },
 })
+
+const customMapStyle = [
+  {
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#1d2c4d',
+      },
+    ],
+  },
+  {
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#8ec3b9',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#1a3646',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.country',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#4b6878',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#64779e',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.locality',
+    elementType: 'geometry',
+    stylers: [
+      {
+        visibility: 'on',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.neighborhood',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.province',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#4b6878',
+      },
+    ],
+  },
+  {
+    featureType: 'landscape.man_made',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#334e87',
+      },
+    ],
+  },
+  {
+    featureType: 'landscape.natural',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#023e58',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#283d6a',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#6f9ba5',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#1d2c4d',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.business',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.government',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#023e58',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#3C7680',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#304a7d',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#98a5be',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#1d2c4d',
+      },
+    ],
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        visibility: 'on',
+      },
+    ],
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        visibility: 'on',
+      },
+    ],
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        visibility: 'on',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#2c6675',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#255763',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#b0d5ce',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#023e58',
+      },
+    ],
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'geometry',
+    stylers: [
+      {
+        visibility: 'simplified',
+      },
+    ],
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'labels.text',
+    stylers: [
+      {
+        visibility: 'simplified',
+      },
+    ],
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        visibility: 'simplified',
+      },
+    ],
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        visibility: 'simplified',
+      },
+    ],
+  },
+  {
+    featureType: 'transit',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#98a5be',
+      },
+    ],
+  },
+  {
+    featureType: 'transit',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#1d2c4d',
+      },
+    ],
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#283d6a',
+      },
+    ],
+  },
+  {
+    featureType: 'transit.station',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#3a4762',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#0e1626',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#4e6d70',
+      },
+    ],
+  },
+]
